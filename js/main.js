05caccf8dea2344bbc1c074498b4d95d7365c914
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2024-12-01 12:48:33 +0600

    passing code

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// alert("hello world");
+// console.dir("Hello world");
+// console.log("hello jonogon")
+// document.write("hello world")
+// console.log("hello world")
+
+// variable
+
+// var nam = 'Ashikur Hridoy';
+// var nam = 'saikat'
+// nam = 'Ashikur'
+
+
+// es6 variable declaration
+
+// let x = "hello world";
+// x = "world"
+// console.log(x); 
+
+// constant variable declaration
+
+// const pi = 3.1416;
+// pi = (3.1614);
+// console.log(pi);
+
+// data type
+
+let y = null;
+console.log(y);
+
+let x;
+console.log(x);
